251f6bc46d7f1f58e1b239d72771c8f25fa089b2
Kyle-Close <k.james.close@gmail.com>
2025-10-28 14:05:08 -0700

    socket layer

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var ipEndPoint = new IPEndPoint(IPAddress.Any, 8080);
+TcpListener listener = new(ipEndPoint);
+
+try
+{
+    listener.Start();
+
+    TcpClient handler = await listener.AcceptTcpClientAsync();
+    await using NetworkStream stream = handler.GetStream();
+
+    byte[] byteBuffer = new byte[1024];
+    int bytesRead = await stream.ReadAsync(byteBuffer);
+
+    var msg = Encoding.UTF8.GetString(byteBuffer, 0, bytesRead);
+    Console.WriteLine(msg);
+
+    // var message = $"{DateTime.Now}";
+    // var dateTimeBytes = Encoding.UTF8.GetBytes(message);
+
+    // await stream.WriteAsync(dateTimeBytes);
+    // Console.WriteLine($"Sent message: \"{message}\"");
+}
+finally
+{
+    listener.Stop();
+}
